e3d4fd11c30dda84a5c9f405987d906498174c1d
Meet Pandya <pandyama2@gmail.com>
2020-05-18 19:32:09 -0400

    initial commite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatTable} from '@angular/material/table';
+
+export interface GitRepos{
+  name: string,
+  description: string,
+  url: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  @ViewChild(MatTable) table: MatTable<GitRepos>;
+
   title = 'getrepos';
+  value: string;
+
+  result: any;
+
+  dataSource: any;
+
+  //dataSource = new MatTableDataSource<GitRepos[]>();
+
+  displayedColumns: string[] = ['Repo', 'Description', 'Link'];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void{
+  }
+
+  getRepo(){
+    return this.http.get('http://localhost:3100/gitusers',{
+      headers: {user: this.value}
+    })
+      .subscribe(res => {
+        this.dataSource = [];
+        let i = 0;
+        this.result = [];
+
+        while(res[i] != null){
+          let test: GitRepos = {
+            name: '',
+            description: '',
+            url: ''
+          };
+
+          test.name = res[i].name;
+          test.description = res[i].description;
+          test.url = res[i].svn_url;
+          this.result.push(test);
+
+          i++;
+        }
+
+        this.dataSource = new MatTableDataSource(this.result);
+
+        this.dataSource.data = this.result;
+
+        //this.dataSource = this.result;
+        console.log(this.dataSource);
+        /**
+         * Fields
+         * - name
+         * - description
+         * - url
+         * 
+         */
+
+      })
+  }
+
+  onEnter(val: string){
+    this.value = val;
+    this.getRepo();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MaterialModule } from './material.module';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
